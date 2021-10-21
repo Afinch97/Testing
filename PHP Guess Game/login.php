3f763331199734bf4a3bf3b6252421752ef0dfb3
Afinch97 <49850011+Afinch97@users.noreply.github.com>
2021-10-21 15:56:03 -0400

    Add files via upload

---
+++ PHP Guess Game/login.php
@@ -0,0 +1,93 @@
+<?php session_start(); /* Starts the session */
+require 'inc/header.php';
+	/* Check Login form submitted */
+	if(isset($_POST['Submit'])){
+		/* Define username and associated password array */
+		/* You can change username and associated password array to your pref*/
+		$logins = array('Henry' => '123456','username1' => 'password1','username2' => 'password2');
+
+		/* Check and assign submitted Username and Password to new variable */
+		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+		/* Check Username and Password existence in defined array */
+		if (isset($logins[$Username]) && $logins[$Username] == $Password){
+			/* Success: Set session variables and redirect to Protected page  */
+			$_SESSION['UserData']['Username']=$logins[$Username];
+			header("location:index2.php");
+			exit;
+		} else {
+			/*Unsuccessful attempt: Set error message */
+			$msg="<span style='color:red'>Invalid Login Details</span>";
+		}
+	}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP Login Script Without Using Database</title>
+<style>
+	@import url('https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap');
+	h2 {
+	  font-family: 'Baloo Bhai', cursive;
+	  font-size: 8rem;
+	  text-shadow: 4px 4px 8px #000;
+	}
+	.header {
+      color: #4C85BE;
+    }
+	body {
+	  background-image: url('images/constellate-pFfZDaTVdtc-unsplash.jpg');
+	  background-size: cover;
+
+	}
+	form {
+	  margin-top: -25rem;
+	}
+	input[type=submit] {
+	  box-shadow: 4px 4px 8px #000;
+	}
+</style>
+</head>
+<body>
+<div class="background"></div>
+        <div class="background background2"></div>
+        <div class="background background3"></div>
+<div id="Frame0">
+  <h3 class="header">PHP Login Script Without Using Database Demo.</h3>
+
+</div>
+<br>
+<form action="" method="post" name="Login_Form">
+  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+    <?php if(isset($msg)){?>
+    <tr>
+      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+    </tr>
+    <?php } ?>
+    <tr>
+      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td align="right" valign="top">Username</td>
+      <td><input name="Username" type="text" class="Input"></td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td><input name="Password" type="password" class="Input"></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+	   <td>     <tr> Username  => Henry' Password => '123456'     </tr></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
+<?php
+
+require 'inc/footer.php';
+
+?>
---
+++ PHP Guess Game/play.php
@@ -0,0 +1,119 @@
+<?php
+
+session_start();
+
+if (isset($_POST['start'])) {
+    unset($_SESSION['selected']);
+    unset($_SESSION['phrase']);
+}
+
+if (isset($_SESSION['selected']) && isset($_POST['key'])) {
+    $_SESSION['selected'][] = $_POST['key'];
+} else {
+    $_SESSION['selected'] = [];
+}
+
+include 'inc/Phrase.php';
+include 'inc/Game.php';
+
+if (isset($_SESSION['phrase'])) {
+	$phrase = new Phrase($_SESSION['phrase'], $_SESSION['selected']);
+} else {
+	$phrase = new Phrase();
+	$_SESSION['phrase'] = $phrase->currentPhrase;
+}
+$game = new Game($phrase);
+
+require 'inc/header.php';
+
+?>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap');
+	h2 {
+	  font-family: 'Baloo Bhai', cursive;
+	  font-size: 6rem;
+	  text-shadow: 2px 2px 4px #000;
+    }
+    .header {
+      color: #4C85BE;
+    }
+    body {
+	  background-image: url('images/oldTv.jpg');
+	  background-position: center;
+    background-size: cover;
+
+	}
+    .correct {
+      background: var(--color-win);
+      border-radius: 5px;
+      font-size: var(--font-size-medium);
+      color: #FFFFFF;
+    }
+    .incorrect {
+      background: var(--color-lose);
+      color: #FFFFFF;
+    }
+    button {
+      box-shadow: 0 8px 0 0 #000;
+    }
+    button:active {
+      box-shadow: none;
+      top: 8px;
+      margin-bottom: 0;
+    }
+</style>
+
+
+
+<div class="main-container" id="overlay">
+<h2 class="header animated infinite pulse slower">TV Phrase Hunter</h2>
+<?php if ($game->checkForLose() == true){ ?>
+    <style>
+        body {
+          background: linear-gradient(15deg, #f5785f 50%, 	#FF0000 50.1%);
+          width: 100%;
+          height: 100%;
+          z-index: 1;
+        }
+        .header {
+          color: white;
+        }
+        input[type=submit] {
+	      box-shadow: 4px 4px 8px #000;
+	}
+    </style>
+    <?php echo $game->gameOver();
+    } else if ($game->checkForWin() == true) { ?>
+        <style>
+            body {
+              background: linear-gradient(15deg, #78CF82 50%, #008000 50.1%);
+              width: 100%;
+              height: 100%;
+              z-index: 1;
+            }
+            .header {
+              color: white;
+            }
+            input[type=submit] {
+	          box-shadow: 4px 4px 8px #000;
+	}
+        </style>
+    <?php echo $game->gameOver();
+    } else { ?>
+    <div id="phrase" class="section">
+        <ul>
+            <?php echo $phrase->addPhraseToDisplay(); ?>
+        </ul>
+    </div>
+    <?php echo $game->displayKeyboard(); ?>
+    <div id="scoreboard" class="section">
+        <ol>
+            <?php echo $game->displayScore(); ?>
+        </ol>
+    </div>
+    <?php } ?>
+</div>
+
+<?php
+
+require 'inc/footer.php';
